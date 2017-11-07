a518f1a78e6db8084ff404878274ee8b4443189d
Ali Zafar <zafarsadiq120@gmail.com>
2017-11-07 09:48:06 -0500

    Completed with proper understanding

--- assignment4/mapper.py
+++ assignment4/mapper.py
@@ -2,8 +2,8 @@
 from urlparse import urlparse
 import sys
 for line in sys.stdin:
-	data = line.strip().split(" ")
+	data = line.strip().split("GET ")
 	if len(data) > 1 :
-		pathname = data[6]
+		pathname = data[1].split(" ")[0]
 		pathname=urlparse(pathname).netloc+urlparse(pathname).path
 		print "{0}".format(pathname)
--- assignment5/mapper.py
+++ assignment5/mapper.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python
-
+from urlparse import urlparse
 import sys
 
 for line in sys.stdin:
-	data = line.strip().split(" ")
+	data = line.strip().split("GET ")
 	if len(data) > 1 :
-		file_url_name = data[6]
+		file_url_name = data[1].split(" ")[0]
+		file_url_name=urlparse(file_url_name).netloc+urlparse(file_url_name).path
 		print "{0}".format(file_url_name)
