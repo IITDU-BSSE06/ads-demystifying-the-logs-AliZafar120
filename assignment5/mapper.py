b0a6524eec80c01bead09c10f9bf9c5dcafe88a4
Ali Zafar <zafarsadiq120@gmail.com>
2017-11-07 08:07:15 -0500

    corrected assignment 5 according to my idea. Explanation is in contribution

--- assignment5/mapper.py
+++ assignment5/mapper.py
@@ -3,7 +3,7 @@
 import sys
 
 for line in sys.stdin:
-	data = line.strip().split(" ")
+	data = line.strip().split("GET ")
 	if len(data) > 1 :
-		file_url_name = str(data[6]).strip()
+		file_url_name = data[1].split(" ")[0]
 		print "{0}".format(file_url_name)
